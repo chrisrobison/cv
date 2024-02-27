2bcdcca71e04d28c5c13d086692aacebe76ea4b5
Christopher Robison <cdr@netoasis.net>
2024-02-26 19:37:41 -0800

    Updated genres.php

--- genres.php
+++ genres.php
@@ -1,9 +1,6 @@
 #!/usr/local/bin/php
 <?php
 
-//if (!file_exists("themes.json")) {
-    file_put_contents("themes.json", file_get_contents("https://registry.jsonresume.org/themes"));
-//}
 $themes = json_decode(file_get_contents("themes.json"));
 $newthemes = array();
 
@@ -12,36 +9,15 @@ $html = true;
 
 $skip = array('elite', 'orbit');
 
-foreach ($themes as $theme=>$surl) {
-    $theme = preg_replace("/^jsonresume-theme-/", "", $theme);
+foreach ($themes as $theme) {
     $newthemes[] = $theme;
     
     if (in_array($theme, $skip)) { continue; }
-    $surl = preg_replace("/thomasdavis/", "chrisrobison", $surl);
 
-    $resume = file_get_contents($surl);
-    print $surl."\n";
+    $results = `resume export resumes/{$theme}.html -t ./node_modules/jsonresume-theme-{$theme}/`;
+    //$resume = file_get_contents($surl);
+    print $theme." processed\n";
     
-     
-    if ($html && !file_exists("generated/{$theme}.html")) {
-        try {
-            //$html = file_get_contents("https://registry.jsonresume.org/chrisrobison?theme=" . $theme);
-            //file_put_contents("generated/{$theme}.html", $html);
-            $results = `curl -s -o generated/$theme.html $surl`;
-            //$out = `wget -pk https://registry.jsonresume.org/chrisrobison?theme={$theme}`;
-            // file_put_contents("generated/{$theme}.html", $out);
-            $size = filesize("generated/$theme.html");
-            if ($size < 200) {
-                print "WARNING: Bad or no content returned for $theme. Removing file...\n";
-                $results = `rm generated/$theme.html`;
-            } else {
-                print "Wrote $size bytes to generated/{$theme}.html\n";
-            }
-        } catch (Exception $e) {
-            echo 'Caught exception: ',  $e->getMessage(), "\n";
-        }
-    }
-
     if ($pdf) {
         if (file_exists("resumes/".$theme.".html")) {
             $cmd = "wkhtmltopdf -R 0 -L 0 -B 0 -T 0 --page-size Letter --print-media-type resumes/" . $theme . ".html pdfs/$theme.pdf";
